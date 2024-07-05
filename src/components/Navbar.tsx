494924cbfc91a1adb5aac9366aa13198b8981342
saeedo-khan <smk1995m@gmail.com>
2024-07-06 01:56:18 +0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { logo } from "@/assets";
+import { navLinks } from "@/constant";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <div>
+        <Image src={logo} alt="logo" height={32} width={124} />
+      </div>
+      <ul className="list-none sm:flex hidden justify-end items-start flex-1">
+        {navLinks.map((nav, index) => (
+          <Link
+            key={nav.id}
+            href={`#${nav.id}`}
+            className={`font-poppins text-base cursor-pointer
+                ${index == navLinks.length - 1 ? "mr-0" : "mr-10"}
+            `}
+          >
+            <p>{nav.title}</p>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        rimary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
